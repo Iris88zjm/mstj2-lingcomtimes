4942d92eb6383d35750cd4f9fcfb5b88ca7f538e
Iris88zjm <327055610@qq.com>
2018-03-22 09:56:03 +0800

    add code css

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -28,7 +28,9 @@
                         </div>
                         <div class="entry clear">
                             <label>验证码<span>*</span></label>
-                            <input type="text" id="phone" name="phone" placeholder="">
+                            <input type="text" id="code" name="code" placeholder="" class="code">
+                            <a class="btn btn-small get-code" onclick="getCode(this,1)" id="J_getCode">获取验证码</a>
+                            <a class="btn btn-small reset-code" id="J_resetCode" style="display:none;"><span id="J_second">60</span>秒后重发</a>
                         </div>
                         <input type="hidden" name="c" value="<?php echo $c ?>">
                     </form>
@@ -55,6 +57,12 @@
                         <div class="entry clear">
                             <label>电话<span>*</span></label>
                             <input type="text" id="phone" name="phone" placeholder="">
+                        </div>
+                        <div class="entry clear">
+                            <label>验证码<span>*</span></label>
+                            <input type="text" id="code" name="code" placeholder="" class="code">
+                            <a class="btn btn-small get-code" onclick="getCode(this,2)" id="J_getCode">获取验证码</a>
+                            <a class="btn btn-small reset-code" id="J_resetCode" style="display:none;"><span id="J_second">60</span>秒后重发</a>
                         </div>
                         <input type="hidden" name="c" value="<?php echo $c ?>">
                     </form>
@@ -108,6 +116,66 @@
                     }
                 }
             })
+        }
+
+        /*获取验证码*/
+        var isPhone = 1;
+        function getCode(e,i){
+            checkPhone(i); //验证手机号码
+            if(isPhone){
+                resetCode(i); //倒计时
+            }else{
+                if(i == 1){
+                    $('#form_contenct #phone').focus();                
+                }else{
+                    $('#form_contenct_2 #phone').focus(); 
+                }
+            }
+        }
+
+        //验证手机号码
+        function checkPhone(i){
+            if(i == 1){
+                var phone = $('#form_contenct #phone').val();                
+            }else{
+                var phone = $('#form_contenct_2 #phone').val(); 
+            }
+            var pattern = /^1[0-9]{10}$/;
+            isPhone = 1;
+            if(phone == '') {
+                alert('请输入手机号码');
+                isPhone = 0;
+                return;
+            }
+            if(!pattern.test(phone)){
+                alert('请输入正确的手机号码');
+                isPhone = 0;
+                return;
+            }
+        }
+        //倒计时
+        function resetCode(i){
+            var flag;
+            if(i == 1){
+                flag = '#form_contenct ';          
+            }else{
+                flag = '#form_contenct_2 ';  
+            }
+            $(flag + '#J_getCode').hide();
+            $(flag + '#J_second').html('60');
+            $(flag + '#J_resetCode').show();
+            var second = 60;
+            var timer = null;
+            timer = setInterval(function(){
+                second -= 1;
+                if(second >0 ){
+                    $(flag + '#J_second').html(second);
+                }else{
+                    clearInterval(timer);
+                    $(flag + '#J_getCode').show();
+                    $(flag + '#J_resetCode').hide();
+                }
+            },1000);
         }
     </script>
 </body>
